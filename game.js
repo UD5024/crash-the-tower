eed5cffb8255fdaeb300e684991f169d8014d095
UD5024 <114549337+UD5024@users.noreply.github.com>
2024-09-18 13:56:20 +0800

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var level = 1;
 var bosslevel = 0;
 var bossbuff = [];
 var bufftext = "";
-const bossbufflist = ["神盾", "狂熱"];
+const bossbufflist = ["神盾", "狂熱", "暴力"];
 var chalist = [0];
 var enemyCD = 0;
 var f1CT = 0, f2CT = 0, f3CT = 0, f4CT = 0, f5CT = 0, f6CT = 0; //5, 10, 10, 15, 15, 30
@@ -1462,6 +1462,7 @@ class boss{
             gamectx.closePath();
             if (enemysummonpoint > 350) {enemysummonpoint -= 0.1;}
             send_AoE_kb(x, 1, "E");
+            if (bossbuff.find(buff => buff == "暴力")) {send_AoE_dmg(50, x, 1, "E");}
         }
     }
     takedamage(take){
